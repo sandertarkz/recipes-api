dab266579a3c200cc05ba21db8b9e5ed7b0d94d0
sandertar <sander.taranov@gmail.com>
2025-01-22 14:42:05 +0100

    Fix cors issue

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from typing import Annotated
 from fastapi import Depends, FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from sqlmodel import Session
 from app.database import get_session
 from app.routes import post, user, auth, test
@@ -7,6 +8,14 @@ from app.routes import post, user, auth, test
 
 SessionDep = Annotated[Session, Depends(get_session)]
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth.router)
 app.include_router(user.router)
